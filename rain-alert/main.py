43559a7abcb6048323eeaf294fe6ff2f60a24109
Vishal Kushwaha <133014886+grandeurkoe@users.noreply.github.com>
2023-11-01 10:40:51 +0530

    Update main.py

--- rain-alert/main.py
+++ rain-alert/main.py
@@ -1,20 +1,17 @@
 import requests
 import os
-
-# the twilio library allows us to send SMS.
-from twilio.rest import Client
+import smtplib
 
 # Get your own OpenWeather API by creating a free account at "https://openweathermap.org/"
 # API Keys are required to make API Requests to the OpenWeather API.
 # os.environ[key] allows you to get the value of the environment variable i.e., key
 API_KEY = os.environ['API_KEY']
-MY_LAT = 19.194550
-MY_LON = 73.190819
+MY_LAT = 58.416740
+MY_LON = 39.111390
 is_raining = False
 
-# You need account_sid and auth_token to send SMS using the twilio library.
-account_sid = "AC4f183a5a3527be369675e890c6f08666"
-auth_token = os.environ['AUTH_KEY']
+MY_EMAIL = os.environ["MY_EMAIL"]
+MY_PASSWORD = os.environ["MY_PASSWORD"]
 
 rain_param = {
     'lat': MY_LAT,
@@ -36,16 +33,15 @@ rain_data_hourly = rain_data['hourly']
 for hourly_index in range(0, 12):
     if rain_data_hourly[hourly_index]['weather'][0]['id'] < 700:
         is_raining = True
-        client = Client(account_sid, auth_token)
 
-        message = client.messages \
-            .create(
-            body="It's going to rain today. Remember to bring an ☔.",
-            from_='+13344686603',
-            to= '+' + os.environ['MY_CONTACT']
-        )
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs="testing.meowya@gmail.com", msg="Subject:Rain "
+                                                                                             "alert!\n\nIt's going to"
+                                                                                             " rain today. Remember "
+                                                                                             "to bring an ☔.".encode("utf-8"))
 
-        print(message.status)
         break
 
 if is_raining is False:
